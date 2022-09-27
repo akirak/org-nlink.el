9ff5e1cd20df5a82c5e4cc7821e330c1f20152be
Akira Komamura <akira.komamura@gmail.com>
2022-09-28 01:51:39 +0900

    Add linting

---
+++ .nomake/flake.nix
@@ -0,0 +1,30 @@
+{
+  description =
+    "THIS IS AN AUTO-GENERATED FILE. PLEASE DON'T EDIT IT MANUALLY.";
+  inputs = {
+    compat = {
+      flake = false;
+      type = "git";
+      url = "https://git.sr.ht/~pkal/compat";
+    };
+    consult = {
+      flake = false;
+      owner = "minad";
+      repo = "consult";
+      type = "github";
+    };
+    consult-org-nlink = {
+      flake = false;
+      owner = "akirak";
+      repo = "org-nlink.el";
+      type = "github";
+    };
+    org-nlink = {
+      flake = false;
+      owner = "akirak";
+      repo = "org-nlink.el";
+      type = "github";
+    };
+  };
+  outputs = { ... }: { };
+}
